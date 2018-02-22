af15bb6e2145cc7553e0c00ef4f03ceaf2693069
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-22 11:17:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let S = new Set()
+  let ans = 0
+
+  for (let i=0; i<array.length; i++){
+    S.add(array[i])
+  }
+
+  for (let i=0; i<array.length; i++){
+    if (!S.has(array[i]-1)){
+      let j = array[i]
+      while (S.has(j)){
+        j++
+      }
+      ans = Math.max(ans, j-array[i])
+    }
+  }
+  return ans
 }
